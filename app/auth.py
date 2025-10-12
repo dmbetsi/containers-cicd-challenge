0f3c825a206da8944c5abfd97f3cb32c26f5562e
MBETSI KWEGNA Didier <didier.mbetsi@gmail.com>
2025-10-12 18:55:48 +0000

    troubleshoot CI 2

--- app/auth.py
+++ app/auth.py
@@ -2,7 +2,7 @@ from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 SECRET_KEY = "supersecretkey"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
