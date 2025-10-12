8240889cd20d1c3aa7549a91cabc5b5613a47c4a
MBETSI KWEGNA Didier <didier.mbetsi@gmail.com>
2025-10-12 18:05:54 +0000

    troubleshoot CI 2

--- app/database.py
+++ app/database.py
@@ -1,18 +1,18 @@
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
+from sqlalchemy.pool import StaticPool
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+    # Use shared in-memory database if :memory: is used
+    if DATABASE_URL.endswith(":memory:"):
+        DATABASE_URL = "sqlite://"
+        connect_args.update({"uri": True})
+        DATABASE_URL = "sqlite:///file::memory:?cache=shared"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-
-
-
-def init_db():
-    from . import models
-    Base.metadata.create_all(bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args=connect_args,
+    poolclass=StaticPool if "memory" in DATABASE_URL else None,
+)
